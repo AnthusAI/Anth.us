97cc259c28404c1c1deed697f367b9e066f99d52
Ryan Alyn Porter <rap@endymion.com>
2023-12-05 23:22:50 -0500

    Page description tweak.

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -113,7 +113,7 @@ export const Head = () => {
   return (
     <Seo
       title="Home"
-      description="Depend on proven experts With a history of powering a multi-million dollar ticketing system and seamless integrations across platforms for reliable serverless AI solutions on AWS. "
+      description="Depend on proven experts with a history of operational excellence for reliable serverless AI solutions on AWS."description="Depend on proven experts With a history of powering a multi-million dollar ticketing system and seamless integrations across platforms for reliable serverless AI solutions on AWS. "
       image="serverless-ai-software-solutions.png"
     />
   )
