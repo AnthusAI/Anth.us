63b2b2b21495a868de067ab007d7bbb5f5977c1c
Ryan Alyn Porter <rap@endymion.com>
2023-12-14 21:45:28 -0500

    Resume tweaks.  This needs a lot more work.  Have to start somewhere.

--- src/pages/ryan.js
+++ src/pages/ryan.js
@@ -97,6 +97,50 @@ const RyansPage = ({ data }) => {
             </ul>
           </div>
 
+          <div className="experience">
+            <div className="title">CoolJunkie, LLC</div>
+            <div className="subtitle">TechJunkie</div>
+            <div className="info">
+              <div className="date-range">2002 - 2005</div>
+              <div className="location">Miami Beach, FL</div>
+            </div>
+            <p className="description">
+              Entered into a joint venture with Nick McCabe to develop a nightlife production and media company in Miami Beach, FL.  The successful CoolJunkie.com website with its interactive forums was a pillar of the Miami Beach nightlife scene for years in the days before social media.  The company also produced events like the BBC Radio One Miami party during the Winter Music Conference, and events from artists like Pete Tong, Cedric Gervais and many others.
+            </p>
+          </div>
+
+          <div className="experience">
+            <div className="title">Endymion Corporation</div>
+            <div className="subtitle">President</div>
+            <div className="info">
+              <div className="date-range">1997 - Present</div>
+              <div className="location">Miami Beach, FL</div>
+            </div>
+            <p className="description">
+              Started a small software product company after graduating, based around a web mail application that was popular in the early days of the web, in the days of WebTV and AOL.  Developed, licensed and supported a PERL-based web mail application that was used by thousands of users, and a Java-based enterprise web mail product.
+            </p>
+          </div>
+
+          <div className="experience">
+            <div className="title">University of Florida</div>
+            <div className="subtitle">BS, Computer Science</div>
+            <div className="info">
+              <div className="date-range">1992 - 1997</div>
+            </div>
+            <p className="description">
+              Worked as an undergraduate research assistant for Dr. Gerhard X. Ritter and Dr. Mark S. Schmalz helping with their innovative work on <mark>computer vision</mark> using the Image Algebra framework. With funding from the US Department of Defense. One project's goal was the <mark>automated detection</mark> of land mines using multi-spectral imagery.  An early predecessor of today's visual segmentation models.  The project was incredibly successful despite relying on 2D convolution algorithms, without the benefit of modern visual AI models.  My main job was C++ development.
+            </p>
+            <p className="description">
+              A related computer vision project aimed at detecting water mines by removing distortion in images from the surface of the ocean. 
+            </p>
+            <p className="description">
+              Also worked on other research projects, including work by Dr. Panos Livadas on automated code analysis and refactoring tools that foreshadowed LLM-based coding tools like GitHub Copilot.
+            </p>
+            <p className="description">
+              Also worked as an undergraduate teaching assistant for a class based on the classic Lisp textbook, The Structure and Interpretation of Computer Programs.
+            </p>
+          </div>
+
           <h2>Portfolio</h2>
 
           <Solutions
@@ -119,28 +163,6 @@ const RyansPage = ({ data }) => {
             showPreviewImage={false}
             linkToPage={false}
           />
-
-          <h2>Education</h2>
-
-          <div className="experience">
-            <div className="title">University of Florida</div>
-            <div className="subtitle">BS, Computer Science</div>
-            <div className="info">
-              <div className="date-range">1992 - 1997</div>
-            </div>
-            <p className="description">
-              Worked as an undergraduate research assistant for Dr. Gerhard X. Ritter and Dr. Mark S. Schmalz helping with their innovative work on <mark>computer vision</mark> using the Image Algebra framework. With funding from the US Department of Defense. One project's goal was the <mark>automated detection</mark> of land mines using multi-spectral imagery.  An early predecessor of today's visual segmentation models.  The project was incredibly successful despite relying on 2D convolution algorithms, without the benefit of modern visual AI models.  My main job was C++ development.
-            </p>
-            <p className="description">
-              A related computer vision project aimed at detecting water mines by removing distortion in images from the surface of the ocean. 
-            </p>
-            <p className="description">
-              Also worked on other research projects, including work by Dr. Panos Livadas on automated code analysis and refactoring tools that foreshadowed LLM-based coding tools like GitHub Copilot.
-            </p>
-            <p className="description">
-              Also worked as an undergraduate teaching assistant for a class based on the classic Lisp textbook, The Structure and Interpretation of Computer Programs.
-            </p>
-          </div>
 
         </div>
       </article>
