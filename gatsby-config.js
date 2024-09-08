1a24877a74fda935466d6d09ff5c9e1a6a2c54c7
Ryan Alyn Porter <rap@endymion.com>
2024-09-08 17:59:54 -0400

    Build fix.

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,13 +104,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts`,
-      },
-    },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
